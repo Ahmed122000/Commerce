a4c4bd0badd8b8d21ca4b5db80747e47bd326132
Ahmed Hesham <ahmedhesham122000@gmail.com>
2025-09-28 21:52:45 +0300

    fix images upload path problem

--- commerce/urls.py
+++ commerce/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 
 from . import settings
 from django.conf.urls.static import static
-
+from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls"))
