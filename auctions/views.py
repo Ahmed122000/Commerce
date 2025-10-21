12d6df3621eb6bff31bbeb2a40c7f018e5cdc31e
Ahmed Hesham <ahmedhesham122000@gmail.com>
2025-10-21 13:04:21 +0300

    fix the index page to show only active listings

--- auctions/views.py
+++ auctions/views.py
@@ -2,17 +2,18 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.urls import reverse
 from .forms import ListingForm
-from .models import User, Listing, Category
-from .utils import get_active_listings
+from .models import User, Listing, Category, Bid
+from django.utils import timezone
 
 
 def index(request):
     return render(request, "auctions/index.html", {
         'header': "Active Listings", 
-        'listings':get_active_listings(Listing.objects.all())
+        'listings':Listing.objects.filter(end_time__gt=timezone.now())
         }
     )
 
@@ -110,6 +111,30 @@ def list_category(request, category):
         }
     )
 
+@login_required
+def place_bid(request, item_id):
+    if request.method == 'POST':
+        bid_amount = request.POST['bid_amount']
+        listing = Listing.objects.get(id=item_id)
+        bidder = request.user
+        
+        try:
+            bid_amount = float(bid_amount)
+            if bid_amount <= listing.current_price:
+                messages.error(request, "Bid must be higher than current price")
+                return redirect('item', id=item_id)
+
+            bid = Bid.objects.create(amount = bid_amount, listing = listing, bidder = bidder)
+            bid.save()
+
+            messages.success(request, "Bid placed succeessfully")
+            return redirect('item', id=item_id)
+        except(ValueError, TypeError):
+            messages.error(request, "Invalid bid amount")
+            return redirect('item', id=item_id)
+
+    return redirect('item', id=item_id)
+        
 
 @login_required
 def watchlist(request):
